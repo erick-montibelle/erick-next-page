33708bb7eea325551e6e7b67b48fa7d626aa7b86
erick-montibelle <148277938+erick-montibelle@users.noreply.github.com>
2023-10-19 17:00:18 -0300

    aprendi a ussar logica

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
 import Image from 'next/image'
 
 export default function Home() {
+  let preçoPicanha = 10
+  let quantidade = 10
   return (
-    <h1>hello world</h1>
+    <h1>
+      <p>
+        preço kg picanha: {preçoPicanha}
+      </p>
+      <p>
+        eu comprei {quantidade} kg
+      </p>
+      <p>
+        e eu vou pagar: {quantidade*preçoPicanha}
+      </p>
+    </h1>
   )
 }
